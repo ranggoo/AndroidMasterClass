4c33235e4e1eaa5cf9218923cef75e1e6d044e86
john.yelangu <ssu1019@naver.com>
2021-11-26 17:25:18 +0900

    테스트 주석 작성.

--- app/src/main/java/com/ranggoo/janmangruffy/Rf5Activity.kt
+++ app/src/main/java/com/ranggoo/janmangruffy/Rf5Activity.kt
@@ -3,6 +3,7 @@ package com.ranggoo.janmangruffy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 다섯번째 클래스 입니다.
 class Rf5Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
