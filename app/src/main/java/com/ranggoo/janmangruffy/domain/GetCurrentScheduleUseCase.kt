d91b41fcb1c78cfb22185f2d7d116d96bdb2d5fd
amy <melow2@naver.com>
2022-01-21 15:05:53 +0900

    #5 GetCurrentScheduleUseCase 작성.

---
+++ app/src/main/java/com/ranggoo/janmangruffy/domain/GetCurrentScheduleUseCase.kt
@@ -0,0 +1,12 @@
+package com.ranggoo.janmangruffy.domain
+
+import com.ranggoo.janmangruffy.data.EpisodeResponse
+import javax.inject.Inject
+
+class GetCurrentScheduleUseCase @Inject constructor(
+    val repository: TvMazeRepository
+) {
+    suspend operator fun invoke(country: String, date: String): List<EpisodeResponse> {
+        return repository.getCurrentSchedule(country, date)
+    }
+}
